69a491332404979e3d816b392a4c0c514fa3ba56
Pramod581 <pnm654321@gmail.com>
2025-12-07 13:46:16 +0530

    added palyload

--- terraform/main.tf
+++ terraform/main.tf
@@ -98,7 +98,8 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
   api_id                 = aws_apigatewayv2_api.http_api.id
   integration_type       = "AWS_PROXY"
   integration_uri        = aws_lambda_function.crud_lambda.invoke_arn
-  payl
+  payload_format_version = "2.0"
+}
 
 
 
